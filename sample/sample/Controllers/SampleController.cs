119d64e7f0d17a02833fb01237f6156729f98dfa
Jakub Porębski <kuba.porebski3@gmail.com>
2020-01-11 23:28:12 +0100

    Sample project

---
+++ sample/sample/Controllers/SampleController.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Rebar.Core.Command;
+using Rebar.Core.Controller;
+using Rebar.Core.Query;
+using sample.Commands;
+using sample.Queries;
+
+namespace sample.Controllers
+{
+    [ApiController]
+    public class SampleController : Controller
+    {
+        private ICommandDispatcher _commandDispatcher;
+        private IQueryDispatcher _queryDispatcher;
+
+        private const string _NAME = "Bob";
+
+        public SampleController(ICommandDispatcher commandDispatcher, IQueryDispatcher queryDispatcher)
+        {
+            this._commandDispatcher = commandDispatcher;
+            this._queryDispatcher = queryDispatcher;
+        }
+
+        //EXAMPLE COMMAND IMPLEMENTATION
+        [HttpPost]
+        public async Task<IActionResult> SampleCommand()
+        {
+            var command = new SampleCommand(_NAME);
+
+            return await this.HandleRequest(_commandDispatcher, command);
+        }
+
+        //EXAMPLE QUERY IMPLEMENTATION
+        [HttpGet]
+        public async Task<IActionResult> SampleQuery()
+        {
+            var query = new SampleQuery(_NAME);
+
+            return await this.HandleResponse<SampleQuery, SampleResponse>(_queryDispatcher, query);
+        }
+        
+    }
+}
---
+++ sample/sample/Queries/SampleQuery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Rebar.Core.Query;
+
+namespace sample.Queries
+{
+    public class SampleQuery : IQuery<SampleResponse>
+    {
+        public string Name { get; set; }
+
+        public SampleQuery(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
